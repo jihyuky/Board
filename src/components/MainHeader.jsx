b4df56b22b716bec0e0dd41d9c4e3fdae68b3d6c
jihyuky <79386734+jihyuky@users.noreply.github.com>
2023-12-23 11:38:15 -0500

    Changing some formatting

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -8,7 +8,7 @@ function MainHeader() {
     <header className={classes.header}>
       <h1 className={classes.logo}>
         <MdMessage />
-        React Poster
+        Secret Thoughts
       </h1>
       <p>
         <Link to="/create-post" className={classes.button}>
